4df70d51d52b1d9a06200dcdadd6f85fdb368959
estein1988 <estein1988@gmail.com>
2020-10-04 16:23:23 -0600

    Strengthen params.

--- app/models/financial.rb
+++ app/models/financial.rb
@@ -1,5 +1,5 @@
 class Financial < ApplicationRecord
   belongs_to :business
 
-  validates :sales_revenue, :service_revenue, numericality: {greater_than_or_equal_to: 0, less_than_or_equal_to: 100000, message: "must be a number 0 to 10,000,000"}
+  validates :sales_revenue, :service_revenue, :advertising, :cogs, :employee_benefits, :equipment, :insurance, :maintenance, :office_supplies, :rent, :r_and_d, :salaries, :software, :travel, :utilities, :website, :other_expense, :interest, :taxes, :ammoritization, :depreciation, :presence => true, numericality: {greater_than_or_equal_to: 0, less_than_or_equal_to: 10000000, message: "must be a number 0 to 9,999,999"}
 end
